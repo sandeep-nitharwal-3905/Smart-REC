ca2b121b4f562fdbb5ff7e820b31f1de00e6f123
Sandeep Nitharwal <sandeepnitharwal3905@gmail.com>
2025-02-16 17:24:42 +0530

    update

--- src/components/server.js
+++ src/components/server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const cors = require("cors");
-const axios = require("axios");
-const cheerio = require("cheerio");
+import express from "express";
+import cors from "cors";
+import axios from "axios";
+import * as cheerio from "cheerio";
 
 const app = express();
 app.use(cors());
